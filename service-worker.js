79020a6bf39349f2587bc6e7d37ca5d99e69feba
yuu <yuuiti.htnk167@gmail.com>
2026-05-01 12:03:45 +0900

    Vercelより移行

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "bodylog-shell-v3-20251229-pages-path";
+const CACHE_NAME = "bodylog-shell-v3-20260501-icons";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
+  "./icons/icon-180.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
