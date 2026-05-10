9e0ef1f809937c2185e6d6fe281c64f22438543a
yuu <yuuiti.htnk167@gmail.com>
2026-05-10 18:02:50 +0900

    GitHub Pages用にbuild済みファイルを配置

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "bodylog-shell-v14-20260510-github-pages-base";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith((async () => {
+    const cached = await caches.match(req);
+
+    try {
+      const res = await fetch(req);
+      const sameOrigin = new URL(req.url).origin === self.location.origin;
+      if (sameOrigin && res.ok) {
+        const copy = res.clone();
+        const cache = await caches.open(CACHE_NAME);
+        await cache.put(req, copy);
+      }
+      return res;
+    } catch (e) {
+      if (cached) return cached;
+      throw e;
+    }
+  })());
+});
